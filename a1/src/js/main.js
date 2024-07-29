c3ba813c762e86c25f43f63dfa2f66fcb604523e
Harrison Black <harrison.black18@gmail.com>
2024-07-29 16:49:37 +1000

    Drop down nav

--- a1/src/js/main.js
+++ a1/src/js/main.js
@@ -1,3 +1,23 @@
-document.getElementById("select-nav").onchange = () => { console.log("select-nav changed") }
+var sel = document.getElementById("select-nav");
+
+sel.onchange = () => {
+	switch (sel.value) {
+		case "Home":
+			window.location.href = "../html/index.html";
+			break;
+		case "Pets":
+			window.location.href = "../html/pets.html";
+			break;
+		case "Add More":
+			window.location.href = "../html/add.html";
+			break;
+		case "Gallery":
+			window.location.href = "../html/gallery.html";
+			break
+	}
+}
+
+
+
 document.getElementById("search-nav").onchange = () => { console.log("search-nav changed") }
 document.getElementById("search-nav-btn").onclick = () => { console.log("search-nav-btn clicked") }
