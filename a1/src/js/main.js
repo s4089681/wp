4df526dbf4db23eb0bb5c5b73b145bf1c94e6680
Harrison Black <harrison.black18@gmail.com>
2024-07-29 16:02:52 +1000

    onchange functions for html

---
+++ a1/src/js/main.js
@@ -0,0 +1,3 @@
+document.getElementById("select-nav").onchange = () => { console.log("select-nav changed") }
+document.getElementById("search-nav").onchange = () => { console.log("search-nav changed") }
+document.getElementById("search-nav-btn").onclick = () => { console.log("search-nav-btn clicked") }
