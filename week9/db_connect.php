d80394c984a3123b5b81f0d31dedf123fa79570d
Harrison Black <harrison@Harrisons-MacBook-Pro.local>
2024-09-23 15:30:07 +1000

    Change to prod db

--- week9/db_connect.php
+++ week9/db_connect.php
@@ -1,18 +1,18 @@
 <?php
 
 	// vars to connect to prod db
-	/*$db_server = 'talsprddb02.int.its.rmit.edu.au';*/
-	/*$db_user = 's4089681';*/
-	/*$db_pass = 'Sj@119769b';*/
-	/*$db_name = 's4089681';*/
-	/*$conn = "";*/
+	$db_server = 'talsprddb02.int.its.rmit.edu.au';
+	$db_user = 's4089681';
+	$db_pass = 'Sj@119769b';
+	$db_name = 's4089681';
+	$conn = "";
 
 	// vars to connect to local XAMPP
-	$db_server = "localhost";
-	$db_user = "root";
-	$db_pass = "";
-	$db_name = "destination";
-	$conn = "";
+	/*$db_server = "localhost";*/
+	/*$db_user = "root";*/
+	/*$db_pass = "";*/
+	/*$db_name = "destination";*/
+	/*$conn = "";*/
 
 	// Stores connection object in local session storage
 	$_SESSION["conn"] = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
