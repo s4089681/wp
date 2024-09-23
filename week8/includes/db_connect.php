1c8bbe4acdf1707a3ccc9b3e8c50231bd865fb4e
Harrison Black <harrison@Harrisons-MacBook-Pro.local>
2024-09-23 14:59:02 +1000

    Week 8 PHP SQL

--- week8/includes/db_connect.php
+++ week8/includes/db_connect.php
@@ -2,9 +2,10 @@
 
 	// vars to connect to prod db
 	/*$db_server = 'talsprddb02.int.its.rmit.edu.au';*/
-	/*$user = 's4089681';*/
-	/*$password = 'Sj@119769b';*/
-	/*$database = 's4089681';*/
+	/*$db_user = 's4089681';*/
+	/*$db_pass = 'Sj@119769b';*/
+	/*$db_name = 's4089681';*/
+	/*$conn = "";*/
 
 	$db_server = "localhost";
 	$db_user = "root";
@@ -15,9 +16,18 @@
 	$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
 
 	if ($conn) {
-		echo "connected";
+		echo "connected<br>";
 	} else {
-		echo "couldnt connect";
+		echo "couldnt connect<br>";
 	}
+	
+	// Stores and Sends SQL request to server
+	$sql = "SELECT * FROM country";
+	$result = $conn->query($sql);
+
+	// Iterates over SQL response printing each row in the table on a new line
+	while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["countryid"]. " - Name: " . $row["countryname"]. " - Description: " . $row["description"]. "<br>";
+  }
 
 ?>
