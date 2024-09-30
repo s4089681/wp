669d41cee7508226ae3dac52a72338d66da28f64
Harrison Black <harrison@Mac.modem>
2024-09-30 21:30:42 +1000

    Fix page navigation

--- a2/js/nav.js
+++ a2/js/nav.js
@@ -1,29 +1,29 @@
 var sel = document.getElementById("nav-select");
 
 document.getElementById("logo").onclick = () => {
-  window.location.href = "./index.php";
+	window.location.href = "./index.php";
 };
 
 sel.onchange = () => {
-  switch (sel.value) {
-    case "Home":
-      window.location.href = "/a2/php/pages/index.php";
-      break;
-    case "Pets":
-      window.location.href = "/a2/php/pages/pets.php";
-      break;
-    case "Add More":
-      window.location.href = "/a2/php/pages/add.php";
-      break;
-    case "Gallery":
-      window.location.href = "/a2/php/pages/gallery.php";
-      break;
-  }
+	switch (sel.value) {
+		case "Home":
+			window.location.href = "~s4089681/wp/a2/php/pages/index.php";
+			break;
+		case "Pets":
+			window.location.href = "~s4089681/wp/a2/php/pages/pets.php";
+			break;
+		case "Add More":
+			window.location.href = "~s4089681/wp/a2/php/pages/add.php";
+			break;
+		case "Gallery":
+			window.location.href = "~s4089681/wp/a2/php/pages/gallery.php";
+			break;
+	}
 };
 
 document.getElementById("search-nav").onchange = () => {
-  console.log("search-nav changed");
+	console.log("search-nav changed");
 };
 document.getElementById("nav-search").onclick = () => {
-  console.log("search-nav-btn clicked");
+	console.log("search-nav-btn clicked");
 };
