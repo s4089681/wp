a43470f69be7703bc8062c13862c4689394f4a6d
Harrison Black <harrison@Mac.modem>
2024-09-30 21:25:06 +1000

    Fix db password

--- a2/php/includes/db_connect.php
+++ a2/php/includes/db_connect.php
@@ -1,22 +1,22 @@
 <?php
 if (strstr($_SERVER["SERVER_NAME"], "localhost")) {
-  // Displays errors only on local db, not prod
-  ini_set("display_errors", 1);
-  ini_set("display_startup_errors", 1);
-  error_reporting(E_ALL);
+	// Displays errors only on local db, not prod
+	ini_set("display_errors", 1);
+	ini_set("display_startup_errors", 1);
+	error_reporting(E_ALL);
 }
 
 //Check what server you are accessing
 if (strstr($_SERVER["SERVER_NAME"], "localhost")) {
-  $servername = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname = "petsvictoria";
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "petsvictoria";
 } else {
-  $servername = "talsprddb02.int.its.rmit.edu.au";
-  $username = "s123456";
-  $password = "your password";
-  $dbname = "s123456";
+	$servername = "talsprddb02.int.its.rmit.edu.au";
+	$username = "s123456";
+	$password = "Sj@119769b";
+	$dbname = "s123456";
 }
 
 // Create connection
@@ -24,6 +24,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+	die("Connection failed: " . $conn->connect_error);
 }
-?>
