f90ca1fd042ea5ad91042c1904a828370516b1c8
Harrison Black <harrison@Mac.modem>
2024-10-01 12:19:13 +1000

    Fix space in id declaration

--- a2/php/pages/pets.php
+++ a2/php/pages/pets.php
@@ -5,51 +5,51 @@ include "../includes/nav.php";
 ?>
 
 <main id="main">
-		<div id="content">
+	<div id="content">
 
-			<div id="petsHeading">
-				<h1>Discover Pets Victoria</h1>
-				<p>PETS VICTORIA IS A DEDICATED PET ADOPTION ORGANIZATION BASED IN VICTORIA, AUSTRALIA, FOCUSED ON PROVIDING A
-					SAFE AND LOVING ENVIRONMENT FOR PETS IN NEED. WITH A COMPASSIONATE APPROACH, PETS VICTORIA WORKS TIRELESSLY TO
-					RESCUE, REHABILITATE, AND REHOME DOGS, CATS, AND OTHER ANIMALS. THEIR MISSION IS TO CONNECT THESE DESERVING
-					PETS
-					WITH CARING INDIVIDUALS AND FAMILIES, CREATING LIFELONG BONDS, THE ORGANIZATION OFFERS A RANGE OF SERVICES,
-					INCLUDING ADOPTION COUNSELING, PET
-					EDUCATTON, AND CONMONUY SUPPORU PROSRANS, ALL AIMED AU PROMOLING RESPONSIBLE PEL OWNERSHIP AND REDUCNO THE
-					NOMBER OF HOMELESS ANIMALS.</p>
-			</div>
-
-			<div id="imgTable">
-				<img id="petImage" src="../../images/assets/pets.jpeg" alt="">
-				<table id="petTable">
-					<tr>
-						<th id="pet">Pet</th>
-						<th id="type">Type</th>
-						<th id="age">Age</th>
-						<th id="location">Location</th>
-					</tr>
-					<?php
-     $sql = "SELECT * FROM pets";
-     $res = $conn->query($sql);
-     if (mysqli_num_rows($res) > 0) {
-       while ($row = $res->fetch_assoc()) { ?>
-         <tr>
-					<td>
-						
-<a href="../pages/details.php?id= <?php echo $row["petid"]; ?>"><?php echo $row["petname"]; ?></a>
-
-					</td>
-						<td><?php echo $row["type"]; ?></td>
-						<td><?php echo $row["age"]; ?> months</td>
-						<td><?php echo $row["location"]; ?></td>
-					</tr>
-					<?php }
-     }
-     ?>
-				</table>
-			</div>
+		<div id="petsHeading">
+			<h1>Discover Pets Victoria</h1>
+			<p>PETS VICTORIA IS A DEDICATED PET ADOPTION ORGANIZATION BASED IN VICTORIA, AUSTRALIA, FOCUSED ON PROVIDING A
+				SAFE AND LOVING ENVIRONMENT FOR PETS IN NEED. WITH A COMPASSIONATE APPROACH, PETS VICTORIA WORKS TIRELESSLY TO
+				RESCUE, REHABILITATE, AND REHOME DOGS, CATS, AND OTHER ANIMALS. THEIR MISSION IS TO CONNECT THESE DESERVING
+				PETS
+				WITH CARING INDIVIDUALS AND FAMILIES, CREATING LIFELONG BONDS, THE ORGANIZATION OFFERS A RANGE OF SERVICES,
+				INCLUDING ADOPTION COUNSELING, PET
+				EDUCATTON, AND CONMONUY SUPPORU PROSRANS, ALL AIMED AU PROMOLING RESPONSIBLE PEL OWNERSHIP AND REDUCNO THE
+				NOMBER OF HOMELESS ANIMALS.</p>
 		</div>
-	</main>
+
+		<div id="imgTable">
+			<img id="petImage" src="../../images/assets/pets.jpeg" alt="">
+			<table id="petTable">
+				<tr>
+					<th id="pet">Pet</th>
+					<th id="type">Type</th>
+					<th id="age">Age</th>
+					<th id="location">Location</th>
+				</tr>
+				<?php
+				$sql = "SELECT * FROM pets";
+				$res = $conn->query($sql);
+				if (mysqli_num_rows($res) > 0) {
+					while ($row = $res->fetch_assoc()) { ?>
+						<tr>
+							<td>
+
+								<a href="../pages/details.php?id=<?php echo $row["petid"]; ?>"><?php echo $row["petname"]; ?></a>
+
+							</td>
+							<td><?php echo $row["type"]; ?></td>
+							<td><?php echo $row["age"]; ?> months</td>
+							<td><?php echo $row["location"]; ?></td>
+						</tr>
+				<?php }
+				}
+				?>
+			</table>
+		</div>
+	</div>
+</main>
 
 <?php include "../includes/footer.php";
 ?>
