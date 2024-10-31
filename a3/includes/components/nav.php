8fb91a9d44e31becad469ed2adb468f28b140740
Harrison Black <harrison@Harrisons-MacBook-Pro.local>
2024-11-01 09:24:46 +1100

    nav username update

--- a3/includes/components/nav.php
+++ a3/includes/components/nav.php
@@ -27,8 +27,11 @@
 				<?php
 
 				session_start();
-				$username = "";
-				$username = $_SESSION['username'];
+				if (isset($_SESSION['username'])) {
+					$username = $_SESSION['username'];
+				} else {
+					$username = "";
+				}
 
 				if (isset($_SESSION['userID'])) {
 					echo '	<li class="nav-item">';
